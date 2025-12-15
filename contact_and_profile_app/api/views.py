5bdbb12760cd3b3d24a48ed1e4d9e55c1cc758f6
Jonas Stiefer <neo.stief.jon@gmail.com>
2025-12-15 20:52:47 +0100

    add profile view and serializers

--- contact_and_profile_app/api/views.py
+++ contact_and_profile_app/api/views.py
@@ -1,7 +1,25 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status, generics, viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
+from .serializers import ProfileSerializer
+from auth_app.models import CustomUserProfile as User
+
 
 class ProfileView(generics.RetrieveUpdateAPIView):
-    pass
+    queryset = User.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        user = self.request.user
+        profile = get_object_or_404(queryset, id=user.id)
+        return profile
+    
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+    
+    def patch(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
